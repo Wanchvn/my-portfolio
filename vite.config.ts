f461c3f7ba0744a82617199acf6896732bdb615f
wanchvn <mercy@4805@gmail.com>
2026-04-26 16:10:07 +0000

    fix build path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/my-portfolio/",   // ✅ ADD THIS LINE
+  base: "/",   // ✅ ADD THIS LINE
 
   server: {
     host: "::",
